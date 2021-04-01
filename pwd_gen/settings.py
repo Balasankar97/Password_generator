f0fbeba36e74e038cbd743e9416485338e12af19
Balasankar97 <48668777+Balasankar97@users.noreply.github.com>
2021-04-01 16:26:50 +0530

    Update settings.py

--- pwd_gen/settings.py
+++ pwd_gen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k7qct)f-c+0$yx&%tfq02qt)idg9iyf9hv1m_3he#etvi#j+$)'
+SECRET_KEY = '**************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
